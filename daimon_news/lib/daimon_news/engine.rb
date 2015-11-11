60dd6d9e6e5df3da062269b87c3dbe06ac04d414
Ryunosuke SATO <tricknotes.rs@gmail.com>
2015-11-11 16:19:47 +0900

    Add engine migration path to application

--- daimon_news/lib/daimon_news/engine.rb
+++ daimon_news/lib/daimon_news/engine.rb
@@ -1,5 +1,9 @@
 module DaimonNews
   class Engine < ::Rails::Engine
     isolate_namespace DaimonNews
+
+    config.before_initialize do
+      ActiveRecord::Tasks::DatabaseTasks.migrations_paths += DaimonNews::Engine.paths['db/migrate'].to_a
+    end
   end
 end
